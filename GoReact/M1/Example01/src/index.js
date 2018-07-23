e7fba91fb0fb5ab038ff3fc1881cbc76e2463f56
Murilo Henrique <murilohns@gmail.com>
2018-07-23 08:47:58 -0300

    react-01: add prop-types handler

--- GoReact/M1/Example01/src/index.js
+++ GoReact/M1/Example01/src/index.js
@@ -1,24 +1,38 @@
 import React, { Component, Fragment } from 'react';
 import { render } from 'react-dom'
+import propTypes from 'prop-types';
 
 class Button extends Component {
   render() { //component render function
     return (
       <a href=""
-        onClick={this.props.onClick}>{this.props.title}
+        onClick={this.props.onClick}>{this.props.children}
       </a>
     )
   }
 }
+
+Button.defaultProps = {
+  children: 'Salvar',
+};
+
+Button.propTypes = {
+  onClick: propTypes.func.isRequired, // Declara a propriedade onClick do elemento como obrigatório.
+  children: propTypes.string, // o Children do elemento button deve ser uma string.
+}
+
 class App extends Component { //main app component
-  handleClick() {
+  handleClick() { // declara a função handleClick para App
     alert('Botão Clicado')
   }
   render() { //component render function
     return (
       <Fragment>
         <h1>Hello Rocketseat</h1>
-        <Button title="Meu botãozinho" onClick={this.handleClick} />
+        <Button onClick={() => { //declara uma nova função onclick nesse botão
+          alert('Button ')
+        }} />
+      <Button onClick={this.handleClick}> Enviar </Button> // usa a função handleClick de App. Enviar representa o children 
     </Fragment>
     )
   }
