db242eb51873ac9c2ea8201da676e54a4ea10c77
Murilo Henrique <murilohns@gmail.com>
2018-07-23 08:38:41 -0300

    react-01: add first props example

--- GoReact/M1/Example01/src/index.js
+++ GoReact/M1/Example01/src/index.js
@@ -3,15 +3,22 @@ import { render } from 'react-dom'
 
 class Button extends Component {
   render() { //component render function
-    return <a href="">Salvar</a>
+    return (
+      <a href=""
+        onClick={this.props.onClick}>{this.props.title}
+      </a>
+    )
   }
 }
 class App extends Component { //main app component
+  handleClick() {
+    alert('Botão Clicado')
+  }
   render() { //component render function
     return (
       <Fragment>
         <h1>Hello Rocketseat</h1>
-        <Button />
+        <Button title="Meu botãozinho" onClick={this.handleClick} />
     </Fragment>
     )
   }
