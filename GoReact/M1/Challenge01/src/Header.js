da7fc6d17d5c92163b246df577d523b83c9d12f3
Murilo Henrique <murilohns@gmail.com>
2018-07-24 23:57:26 -0300

    rocketbook: add header component

---
+++ GoReact/M1/Challenge01/src/Header.js
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Rocketbook</h1>
+  </header>
+);
+
+export default Header;
--- GoReact/M1/Challenge01/src/index.js
+++ GoReact/M1/Challenge01/src/index.js
@@ -1,1 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+
+import './main.css';
+
+import Header from './Header';
+
+class App extends Component {
+  render() {
+    return <Header />;
+  }
+}
+
+render(<App />, document.querySelector('[name="app"]'));
