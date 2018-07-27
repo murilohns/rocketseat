df9f899097c6f45139a15e8e44a138b63373c60b
Murilo Henrique <murilohns@gmail.com>
2018-07-27 12:18:12 -0300

    snippetfy: add user register

--- GoNode/M2/AppSnippetfy/app/controllers/authController.js
+++ GoNode/M2/AppSnippetfy/app/controllers/authController.js
@@ -1,7 +1,25 @@
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+
 const signin = (req, res) => res.render('auth/signin');
 const signup = (req, res) => res.render('auth/signup');
+
+const register = async (req, res) => {
+  const { email } = req.body;
+
+  if (await User.findOne({ where: { email } })) {
+    return res.redirect('back');
+  }
+
+  const password = await bcrypt.hash(req.body.password, 5);
+
+  await User.create({ ...req.body, password });
+
+  return res.redirect('/');
+};
 
 module.exports = {
   signin,
   signup,
+  register,
 };
--- GoNode/M2/AppSnippetfy/app/routes.js
+++ GoNode/M2/AppSnippetfy/app/routes.js
@@ -5,5 +5,6 @@ const authController = require('./controllers/authController');
 
 routes.get('/', authController.signin);
 routes.get('/signup', authController.signup);
+routes.post('/register', authController.register);
 
 module.exports = routes;
