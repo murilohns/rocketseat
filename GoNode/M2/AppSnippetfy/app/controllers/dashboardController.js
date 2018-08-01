aa6dfa6257064d9718db05f0e568d0290aeffd42
Murilo Henrique <murilohns@gmail.com>
2018-08-01 13:05:03 -0300

    snippetfy: add categories to dashboard

--- GoNode/M2/AppSnippetfy/app/controllers/dashboardController.js
+++ GoNode/M2/AppSnippetfy/app/controllers/dashboardController.js
@@ -1,5 +1,19 @@
+const { Category, Snippet } = require('../models');
+
+const index = async (req, res, next) => {
+  try {
+    const categories = await Category.findAll({
+      include: [Snippet],
+      where: {
+        UserId: req.session.user.id,
+      },
+    });
+
+    return res.render('dashboard/index', { categories });
+  } catch (err) {
+    next(err);
+  }
+};
 module.exports = {
-  index(req, res) {
-    return res.render('dashboard/index');
-  },
+  index,
 };
