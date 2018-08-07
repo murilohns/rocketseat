bd46cfb8162d7da311343fef7d046c69422217b5
Murilo Henrique <murilohns@gmail.com>
2018-08-07 10:42:38 -0300

    snippetfy: add show snippets at dashboard

--- GoNode/M2/AppSnippetfy/app/controllers/snippetController.js
+++ GoNode/M2/AppSnippetfy/app/controllers/snippetController.js
@@ -1,4 +1,4 @@
-const { Snippet } = require('../models');
+const { Category, Snippet } = require('../models');
 
 const store = async (req, res, next) => {
   try {
@@ -10,12 +10,40 @@ const store = async (req, res, next) => {
     });
 
     req.flash('success', 'Snippet criado com sucesso');
-    return res.redirect(`app/categories/${categoryId}/snippets/${snippet.id}`);
+    return res.redirect(`/app/categories/${categoryId}/snippets/${snippet.id}`);
   } catch (err) {
     return next(err);
   }
 };
 
+const show = async (req, res, next) => {
+  try {
+    const { categoryId, snippetId } = req.params;
+
+    const categories = await Category.findAll({
+      include: [Snippet],
+      where: {
+        UserId: req.session.user.id,
+      },
+    });
+
+    const snippets = await Snippet.findAll({
+      where: { CategoryId: categoryId },
+    });
+
+    const snippet = await Snippet.findById(snippetId);
+
+    return res.render('snippets/show', {
+      activeCategory: categoryId,
+      categories,
+      snippets,
+      currentSnippet: snippet,
+    });
+  } catch (err) {
+    return next();
+  }
+};
 module.exports = {
   store,
+  show,
 };
