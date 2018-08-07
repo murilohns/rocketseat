4af4735e13e9cf2d9ff166bffb42c167192d29f8
Murilo Henrique <murilohns@gmail.com>
2018-08-07 09:24:16 -0300

    snippetfy: add snippets register at dashboard

--- GoNode/M2/AppSnippetfy/app/routes.js
+++ GoNode/M2/AppSnippetfy/app/routes.js
@@ -7,6 +7,7 @@ const guestMiddleware = require('./middlewares/guest');
 const authController = require('./controllers/authController');
 const dashboardController = require('./controllers/dashboardController');
 const categoryController = require('./controllers/categoryController');
+const snippetController = require('./controllers/snippetController');
 
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success');
@@ -35,6 +36,11 @@ routes.get('/app/dashboard', dashboardController.index);
  */
 routes.get('/app/categories/:id', categoryController.show);
 routes.post('/app/categories/create', categoryController.store);
+
+/**
+ * Snippets
+ */
+routes.post('/app/categories/:categoryId/snippets/create', snippetController.store);
 
 routes.use((req, res) => res.render('errors/404'));
 
