06cc785e5a7a238656f6eff5799960d70b003f13
Murilo Henrique <murilohns@gmail.com>
2018-08-08 23:39:10 -0300

    Docfy: rename folder from documentationApp to Docfy

--- GoNode/M2/Docfy/index.js
+++ GoNode/M2/Docfy/index.js
@@ -8,6 +8,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static(path.resolve('app', 'public')));
+
 nunjucks.configure(path.resolve('app', 'views'), {
   autoscape: true,
   express: app,
