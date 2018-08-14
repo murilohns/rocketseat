725becfd284f5e92c3a4cbf5e54bfa4a35b6ea58
Murilo Henrique <murilohns@gmail.com>
2018-08-14 08:46:34 -0300

    docfy: create user model and migration
    Create User model, just run: sequelize db:migrate

---
+++ GoNode/M2/Docfy/database/migrations/20180814113421-create-users.js
@@ -0,0 +1,33 @@
+module.exports = {
+  up: (queryInterface, DataTypes) => queryInterface.createTable('Users', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+  }),
+
+  down: queryInterface => queryInterface.dropTable('Users'),
+};
