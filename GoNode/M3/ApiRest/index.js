6998006c69ff0d6b84d2fedaee649d92822e0856
Murilo Henrique <murilohns@gmail.com>
2018-09-19 09:32:57 -0300

    apirest: add body parser and create user route

--- GoNode/M3/ApiRest/index.js
+++ GoNode/M3/ApiRest/index.js
@@ -1,12 +1,20 @@
 const app = require('express')();
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const bodyParser = require('body-parser');
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
 
 requireDir(dbConfig.modelsPath);
+app.use(bodyParser.json());
 
-const User = mongoose.model('User');
+app.post('/create', async (req, res) => {
+  const User = mongoose.model('User');
+
+  await User.create(req.body);
+
+  res.send();
+});
 
 app.listen(3000);
