44fc266eaf84c3d8346c2c2d46bb24d8be6fe022
Murilo Henrique <murilohns@gmail.com>
2018-10-29 13:14:22 -0300

    apirest: add hook to crypt password on model

--- GoNode/M3/ApiRest/app/models/user.js
+++ GoNode/M3/ApiRest/app/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -27,5 +28,12 @@ const UserSchema = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+UserSchema.pre('save', async function hashPassword (next) {
+  if (!this.isModified('password')) next();
+
+  this.password = await bcrypt.hash(this.password, 8)
+});
+
 
 mongoose.model('User', UserSchema);
