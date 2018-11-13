f843a78824d3e60127fc468465da8466a789e969
Murilo Henrique <murilohns@gmail.com>
2018-11-13 17:36:19 -0200

    facebook-api: setup docker-compose

--- GoNode/M3/facebook-api/index.js
+++ GoNode/M3/facebook-api/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const app = require('express')();
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE_URL);
 app.use(bodyParser.json());
-
 app.use('/api', require('./app/routes'));
 
 app.listen(3000);
