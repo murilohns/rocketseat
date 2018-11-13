a3938ad0d0d0fbaf6c645a0998ac8b409a39a70b
Murilo Henrique <murilohns@gmail.com>
2018-11-13 18:18:22 -0200

    facebook-api: add error handler middleware

--- GoNode/M3/facebook-api/app/routes.js
+++ GoNode/M3/facebook-api/app/routes.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const routes = express.Router();
 
-routes.get('/status', (req, res) => res.send('API Up'));
+routes.get('/status', (req, res, next) => {
+  try {
+    return res.send('API UP');
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = routes;
--- GoNode/M3/facebook-api/index.js
+++ GoNode/M3/facebook-api/index.js
@@ -2,9 +2,16 @@ require('dotenv').config();
 const app = require('express')();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const PrettyError = require('pretty-error');
+
+const prettyError = new PrettyError();
 
 mongoose.connect(process.env.DATABASE_URL);
 app.use(bodyParser.json());
 app.use('/api', require('./app/routes'));
 
+app.use((err, req, res, next) => console.log(prettyError.render(err)));
+
+prettyError.skipNodeFiles();
+prettyError.skipPackage('express');
 app.listen(3000);
